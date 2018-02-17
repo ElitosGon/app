cad1cb23bf8b3f010da65d7927a7b5e1b46c7d78
ElitosGon <elias.gonzalezma@gmail.com>
2018-02-16 22:37:23 -0300

    Add test User model and Register Controller

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "should not save user without email" do
+		user = User.new
+		user.image = 'test'
+		assert_not user.save
+	end
+
+	test "should not save user without image" do
+		user = User.new
+		user.email = 'test'
+		assert_not user.save
+	end
+
+	test 'valid user' do
+		user = User.new(email: 'a@example.com', image:'test string')
+		assert user.valid?
+	end
+
+	test "should be unique user email" do
+		user = User.new(email: 'a@example.com', image:'test string')
+		user.save
+		new_user = User.new(email: 'a@example.com', image:'test string')
+        assert_not new_user.save
+    end
+	
 end
+
